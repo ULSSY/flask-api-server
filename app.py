50dfcf301dd3b5de312fd2facaed1166f42eed01
ULSSY <96038813+ULSSY@users.noreply.github.com>
2022-01-04 12:38:07 +0900

    2021-01-04

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask_restful import Api
 
 from resources.recipe import RecipeListResource
 from resources.recipe_info import RecipeResource
+from resources.recipe_publish import RecipePublishResource
+from resources.register import UserRegisterResource
 
 app = Flask(__name__)
 
@@ -15,6 +17,9 @@ api = Api(app)
 # 경로와 리소스를 연결한다.
 api.add_resource(RecipeListResource, '/recipes')
 api.add_resource(RecipeResource,'/recipes/<int:recipe_id>')
+api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+api.add_resource(UserRegisterResource, '/user/register')
+api.add_resource(UserLoginResource,'/user/login')
 
 if __name__ == "__main__" :
     app.run()
